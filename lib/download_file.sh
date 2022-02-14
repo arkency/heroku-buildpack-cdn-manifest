4fa2e4d7bf5213a7b079450930fb452871035fb1
Szymon Fiedler <szymon.fiedler@gmail.com>
2022-02-14 15:28:31 +0100

    Extract function to hide verbosity
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>

---
+++ lib/download_file.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+download_file () {
+  local aws_binary_path=$1
+  local remote_path=$2
+  local local_path=$3
+
+  AWS_ACCESS_KEY_ID=$(get_env_variable ${env_dir} "AWS_ACCESS_KEY_ID") \
+    AWS_SECRET_ACCESS_KEY=$(get_env_variable ${env_dir} "AWS_SECRET_ACCESS_KEY") \
+    AWS_REGION=$(get_env_variable ${env_dir} "AWS_REGION") \
+    $aws_binary_path s3 cp s3://$(get_env_variable ${env_dir} "AWS_BUCKET")/$remote_path $local_path --quiet
+
+  status = $?
+  if [[ $status -eq 0 ]]; then 
+    echo "Downloaded remote $remote_path to $local_path"
+  fi
+}
