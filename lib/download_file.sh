8edb9f075e163a8ceb084b2b713698a32d4bbb37
Szymon Fiedler <szymon.fiedler@gmail.com>
2022-02-14 17:31:34 +0100

    Refactoring
      * reuse globals to minimize argument list
      * concatenate paths in local variables
    
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>

--- lib/download_file.sh
+++ lib/download_file.sh
@@ -3,17 +3,17 @@
 download_file () {
   local aws_binary_path=$1
   local remote_path=$2
-  local local_path=$3
+  local asset_manifest_path=$(get_env_variable ${env_dir} "ASSET_MANIFEST_PATH")
 
   AWS_ACCESS_KEY_ID=$(get_env_variable ${env_dir} "AWS_ACCESS_KEY_ID") \
     AWS_SECRET_ACCESS_KEY=$(get_env_variable ${env_dir} "AWS_SECRET_ACCESS_KEY") \
     AWS_REGION=$(get_env_variable ${env_dir} "AWS_REGION") \
-    $aws_binary_path s3 cp s3://$(get_env_variable ${env_dir} "AWS_BUCKET")/$remote_path $local_path --quiet
+    $aws_binary_path s3 cp s3://$(get_env_variable ${env_dir} "AWS_BUCKET")/$remote_path $build_dir/$asset_manifest_path --quiet
 
   local status=$?
 
   if [[ status -eq 0 ]]; then
-    echo "Downloaded remote $remote_path to $local_path" | indent
+    echo "Downloaded remote $remote_path to $asset_manifest_path" | indent
   fi
 
   return $status
